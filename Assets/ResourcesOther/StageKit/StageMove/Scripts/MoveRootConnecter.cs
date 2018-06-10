2f0cbae713fa21f6b645487b2797abaaf465440a
KAZUTAKA-PC\kazutaka <ikimasyouyotwitter@gamil.com>
2018-06-10 23:23:01 +0900

    スケールのxyz比率が1:1:1でないオブジェクトに触れた際にキャラクターのスケールがおかしくなるバグを修正 厳密には床オブジェクトの仕様変更 ・床にするオブジェクトはコライダーを管理する親オブジェクトとメッシュを管理する子オブジェクトの構成とする ・親オブジェクトのスケール比率は常に1:1:1とする ・比率を変更する場合は子オブジェクトのスケールと、親オブジェクトのコライダーのスケールを変更する

---
+++ Assets/ResourcesOther/StageKit/StageMove/Scripts/MoveRootConnecter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class MoveRootConnecter : MonoBehaviour
+{
+	private LineRenderer m_lineRernderer;
+
+	private Vector3[] m_positions;
+
+	/// <summary>
+	/// 初期化処理
+	/// </summary>
+	public void Init ()
+	{
+		m_lineRernderer = this.GetComponent<LineRenderer>();
+		m_positions = new Vector3[this.transform.childCount];
+		
+		int count = 0;
+		// 子供のトランスフォームを取得
+		foreach (Transform child in transform)
+		{
+			m_positions[count] = child.position;
+			count++;
+		}
+		m_lineRernderer.positionCount = count;
+
+		// ラインレンダラーにポジションをセット
+		m_lineRernderer.SetPositions(m_positions);
+	}
+
+	/// <summary>
+	/// ポジションの配列を返す
+	/// </summary>
+	public Vector3[] GetPositions()
+	{
+		return m_positions;
+	}
+}
---
+++ Assets/ResourcesOther/StageKit/StageMove/Scripts/StageMoveManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageMoveManager : MonoBehaviour
+{
+	[SerializeField]
+	private MoveRootConnecter m_moveRootConnecter;
+
+	[SerializeField]
+	private StageMove m_stageMove;
+
+	// Use this for initialization
+	void Start ()
+	{
+		// 道順を決める
+		m_moveRootConnecter.Init();
+		// 移動させる
+		m_stageMove.Init(m_moveRootConnecter.GetPositions());
+	}
+}
